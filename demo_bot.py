f02b7cac357b20367eeaf278e8b3d7f23df50757
”Daniil <lemk@ukr.net>
2018-02-16 21:05:27 +0200

    new commit

--- demo_bot.py
+++ demo_bot.py
@@ -7,6 +7,7 @@ class BotHandler:
     def __init__(self):
         self.api_url = "https://api.telegram.org/bot498390686:AAEHhlW83TllNrC_IBSEdBhv6LibGE1G2tc/"
 
+
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
@@ -14,11 +15,13 @@ class BotHandler:
         result_json = resp.json()['result']
         return result_json
 
+
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
         return resp
+
 
     def get_last_update(self):
         get_result = self.get_updates()
@@ -31,10 +34,13 @@ class BotHandler:
         return last_update
 
 
+
 greet_bot = BotHandler()  
 greetings = ('hello', 'hi', 'greetings', 'sup')  
 now = datetime.datetime.now()
  
+
+
 def main():  
     new_offset = None
     today = now.day
@@ -70,41 +76,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
-
-""" 
-url = "https://api.telegram.org/bot498390686:AAEHhlW83TllNrC_IBSEdBhv6LibGE1G2tc/"
-
-
-def get_updates_json(request):
-	params = {'timeout': 100, 'offset': None}
-	response = requests.get(request + 'getUpdates', data=params)
-	return response.json()
-
-
-def last_update(data):
-	results = data['result']
-	total_updates = len(results) - 1
-	return results[total_updates]
-
-
-def get_chat_id(update):
-	chat_id = update['message']['chat']['id']
-	return chat_id
-
-
-def send_mess(chat, text):
-	params = {'chat_id': chat, 'text': text}
-	response = requests.post(url + 'sendMessage', data=params)
-	return response
-
-def main():
-	update_id = last_update(get_updates_json(url))['update_id']
-	while True:
-		if update_id == last_update(get_updates_json(url))['update_id']:
-			send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-	sleep(1)
-
-if __name__ == '__main__':
-	main()
-
-"""
